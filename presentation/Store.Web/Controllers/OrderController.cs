6e2598a6b0c63f4db3d7a88522edbb6a04c54c01
Jmih_Pojiloy <mr.d.pod@mail.ru>
2023-01-22 22:48:07 +0300

    fixed bugs at the presentation level

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -51,7 +51,7 @@ namespace Store.Web.Controllers
             };
         }
 
-        public IActionResult AddItem(int bookId, int count)
+        public IActionResult AddItem(int bookId, int count = 1)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
 
