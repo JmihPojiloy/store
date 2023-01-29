5855c9c6834f5191a2cbe71a0a1ce0d95436596b
Jmih_Pojiloy <mr.d.pod@mail.ru>
2023-01-29 22:31:39 +0300

    add online payment

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Store.Contractors;
 using Store.Messages;
+using Store.Web.Contractors;
 using Store.Web.Models;
 using System.Text.RegularExpressions;
 
@@ -13,12 +14,14 @@ namespace Store.Web.Controllers
         private readonly IOrderRepository orderRepository;
         private readonly IEnumerable<IDeliveryService> deliveryServices;
         private readonly IEnumerable<IPaymentService> paymentServices;
+        private readonly IEnumerable<IWebContractorService> webContractorServices;
         private readonly INotificationService notificationService;
 
         public OrderController(IBookRepository bookRepository,
                               IOrderRepository orderRepository,
                               IEnumerable<IDeliveryService> deliveryServices,
                               IEnumerable<IPaymentService> paymentServices,
+                              IEnumerable<IWebContractorService> webContractorServices,
                               INotificationService notificationService)
         {
             this.bookRepository = bookRepository;
@@ -26,6 +29,7 @@ namespace Store.Web.Controllers
             this.deliveryServices = deliveryServices;
             this.paymentServices = paymentServices;
             this.notificationService = notificationService;
+            this.webContractorServices = webContractorServices;
         }
 
         [HttpGet]
@@ -269,10 +273,19 @@ namespace Store.Web.Controllers
         [HttpPost]
         public IActionResult StartPayment(int id, string uniqueCode)
         {
-            var paymentService = paymentServices.Single(service => service.UniqueCode == uniqueCode);
+            var paymentService = paymentServices.Single(service => 
+                                                        service.UniqueCode == uniqueCode);
             var order = orderRepository.GetById(id);
 
             var form = paymentService.CreateForm(order);
+
+            var webContractorService = webContractorServices.SingleOrDefault(service => 
+                                                                             service.UniqueCode == uniqueCode);
+
+            if(webContractorService != null)
+            {
+                return Redirect(webContractorService.GetUri);
+            }
 
             return View("PaymentStep", form);
         }
@@ -296,5 +309,11 @@ namespace Store.Web.Controllers
             return View("PaymentStep", form);
         }
 
+        public IActionResult Finish()
+        {
+            HttpContext.Session.RemoveCart();
+
+            return View();
+        }
     }
 }
