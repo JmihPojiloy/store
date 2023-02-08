2e7eeedaae38968dc46c65037f82ca431a260a11
Jmih_Pojiloy <mr.d.pod@mail.ru>
2023-02-08 22:00:28 +0300

    add exception filter

--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -1,6 +1,7 @@
 using Store.Contractors;
 using Store.Data.EF;
 using Store.Messages;
+using Store.Web;
 using Store.Web.App;
 using Store.Web.Contractors;
 using Store.YandexKassa;
@@ -9,7 +10,11 @@ using System.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 // AddOrUpdateItem services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(typeof(ExceptionFilter));
+});
+
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
@@ -32,7 +37,8 @@ builder.Services.AddSingleton<OrderService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+//if (!app.Environment.IsDevelopment())
+if(false)
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
